133fb1ce7fa4a21406f6ec7ab764e3578b1f38de
bmeistergeneral <brecon.morgan@gmail.com>
2024-03-17 13:43:28 -0400

    Removed duplicate properties

--- ElectronicDevice.h
+++ ElectronicDevice.h
@@ -5,10 +5,6 @@
 class ElectronicDevice {
 
 public:
-    std::string manufacturerName;
-    std::string modelName;
-    std::string serialNumber;
-  
     std::string getManufacturerName();
     std::string getModelName();
     std::string getSerialNumber();
